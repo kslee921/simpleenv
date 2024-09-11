2a400d30753c37e77739b9ebed82455a08e50eae
kslee921 <61579820+kslee921@users.noreply.github.com>
2024-09-12 00:44:06 +0800

    Update s3.tf

--- terraform/simple_instance/s3.tf
+++ terraform/simple_instance/s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket" "prismaclouds3" {
   bucket_prefix = "prismacloud-s3"
 
   tags = {
-    Name                 = "Prisma Cloud"
+    Name                 = "Prisma Cloud-KS"
     Environment          = "Dev"
     Key          = "c39bbe99-7fkd-422a-99a9-d551448f0222"
     Secret       ="ekh/VxZLQ7UrJsX+pdV/00AD0/Z="
